44bb366beb341993788a55c2d32d3e9891f19605
alejandro <alejandrohp1999@gmail.com>
2024-03-11 12:28:41 -0600

    agregando campos

--- app_movil/lib/main.dart
+++ app_movil/lib/main.dart
@@ -21,6 +21,18 @@ class CameraApp extends StatefulWidget {
 class _CameraAppState extends State<CameraApp> {
   late CameraController controller;
 
+  final TextEditingController nombreController = TextEditingController();
+  final TextEditingController apellidoPaternoController =
+      TextEditingController();
+  final TextEditingController apellidoMaternoController =
+      TextEditingController();
+  final TextEditingController domicilioController = TextEditingController();
+  final TextEditingController coloniaController = TextEditingController();
+  final TextEditingController codigoPostalController = TextEditingController();
+  final TextEditingController curpController = TextEditingController();
+
+  bool isDetecting = false;
+
   @override
   void initState() {
     super.initState();
@@ -48,53 +60,108 @@ class _CameraAppState extends State<CameraApp> {
     if (controller.value.isInitialized) {
       return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: const Text('Ejemplo de Cámara')),
-          body: AspectRatio(
-            aspectRatio: .7,
-            child: CameraPreview(controller),
+          appBar: AppBar(title: const Text('captura de datos')),
+          body: Column(
+            children: [
+              Stack(
+                children: [
+                  AspectRatio(
+                    aspectRatio:
+                        3 / 2, // Relación de aspecto 2:3 común en las cámaras
+                    child: CameraPreview(controller),
+                  ),
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        margin: EdgeInsets.only(bottom: 16),
+                        child: isDetecting
+                            ? Text(
+                                'Detectando credencial...',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            : SizedBox.shrink(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      _buildTextField("Apellido Paterno", nombreController),
+                      _buildTextField(
+                          "Apellido Materno", apellidoPaternoController),
+                      _buildTextField("Nombre", apellidoMaternoController),
+                      _buildTextField("Domicilio", domicilioController),
+                      _buildTextField("Colonia", coloniaController),
+                      _buildTextField("Código Postal", codigoPostalController),
+                      _buildTextField("CURP", curpController),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
           floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.camera),
             onPressed: () async {
-             try {
-              final image = await controller.takePicture();
-  // final pickedImage =
-  //     await ImagePicker().getImage(source: ImageSource.gallery);
-  // if (pickedImage != null) {
-    final inputImage =
-        InputImage.fromFilePath(image.path);
-    final textRecognizer = GoogleMlKit.vision.textRecognizer();
-    final recognisedText =
-        await textRecognizer.processImage(inputImage);
-    String extractedText = recognisedText.text;
+              try {
+                setState(() {
+                  isDetecting = true;
+                });
 
-    print('Texto reconocido: $extractedText');
+                final image = await controller.takePicture();
+                final inputImage = InputImage.fromFilePath(image.path);
+                final textRecognizer = GoogleMlKit.vision.textRecognizer();
+                final recognisedText =
+                    await textRecognizer.processImage(inputImage);
+                String extractedText = recognisedText.text;
 
-    RegExp nombreRegex = RegExp(r'NOMBRE\n(.+?)\n(.+?)\n(.+?)\n');
-    RegExp domicilioRegex = RegExp(r'DOMICILIO\n(.+?)\n(.+?)\n(.+?)\n');
-    RegExp curpRegex = RegExp(r'CURP (.+)');
+                RegExp nombreRegex = RegExp(r'NOMBRE\n(.+?)\n(.+?)\n(.+?)\n');
+                RegExp domicilioRegex =
+                    RegExp(r'DOMICILIO\n(.+?)\n(.+?)\n(.+?)\n');
+                RegExp curpRegex = RegExp(r'CURP (.+)');
 
-    String? nombre = nombreRegex.firstMatch(extractedText)?.group(1);
-    String? apellidoPaterno = nombreRegex.firstMatch(extractedText)?.group(2);
-    String? apellidoMaterno = nombreRegex.firstMatch(extractedText)?.group(3);
-    String? domicilio = domicilioRegex.firstMatch(extractedText)?.group(1);
-    String? colonia = domicilioRegex.firstMatch(extractedText)?.group(2);
-    String? codigoPostal = domicilioRegex.firstMatch(extractedText)?.group(3);
-    String? curp = curpRegex.firstMatch(extractedText)?.group(1);
+                String? nombre =
+                    nombreRegex.firstMatch(extractedText)?.group(1);
+                String? apellidoPaterno =
+                    nombreRegex.firstMatch(extractedText)?.group(2);
+                String? apellidoMaterno =
+                    nombreRegex.firstMatch(extractedText)?.group(3);
+                String? domicilio =
+                    domicilioRegex.firstMatch(extractedText)?.group(1);
+                String? colonia =
+                    domicilioRegex.firstMatch(extractedText)?.group(2);
+                String? codigoPostal =
+                    domicilioRegex.firstMatch(extractedText)?.group(3);
+                String? curp = curpRegex.firstMatch(extractedText)?.group(1);
 
-    print('Nombre: $nombre $apellidoPaterno $apellidoMaterno');
-    print('Domicilio: $domicilio $colonia $codigoPostal');
-    print('CURP: $curp');
+                setState(() {
+                  nombreController.text = nombre ?? '';
+                  apellidoPaternoController.text = apellidoPaterno ?? '';
+                  apellidoMaternoController.text = apellidoMaterno ?? '';
+                  domicilioController.text = domicilio ?? '';
+                  coloniaController.text = colonia ?? '';
+                  codigoPostalController.text = codigoPostal ?? '';
+                  curpController.text = curp ?? '';
+                  isDetecting = false;
+                });
 
-    textRecognizer.close();
-  // }
-} catch (e) {
-  print(e);
-}
-
-
-
-
+                textRecognizer.close();
+              } catch (e) {
+                print(e);
+                setState(() {
+                  isDetecting = false;
+                });
+              }
             },
           ),
         ),
@@ -103,14 +170,17 @@ class _CameraAppState extends State<CameraApp> {
       return Container();
     }
   }
+
+  Widget _buildTextField(String labelText, TextEditingController controller) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: labelText,
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
 }
-
-
-
-
-
-
-
-
-
-
